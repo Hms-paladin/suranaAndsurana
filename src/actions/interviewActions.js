188ec5478e842a746b0a91b17b7b99733f799ac5
Ranjith <ranjithvbr@gmail.com>
2021-02-25 13:10:27 +0530

    redux example implemented in interview

---
+++ src/actions/interviewActions.js
@@ -0,0 +1,21 @@
+import { GET_INTERVIEW_QUESTIONS } from "../utils/Constants";
+import { apiurl } from "../utils/baseUrl.js";
+import axios from "axios";
+
+
+export const getInterviewquestions = () => async dispatch => {
+    try {
+
+        axios({
+            method: 'GET',
+            url: apiurl +'/get_questions',
+        })
+        .then((response) => {
+            console.log(response.data.data,"datacheck")
+            dispatch({type:GET_INTERVIEW_QUESTIONS,payload:response.data.data})
+        })
+        
+    } catch (err) {
+        
+    }
+}
