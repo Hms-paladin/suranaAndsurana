0907b2311521e84a53a4d07a3aa94fd476165769
Ranjith <birundhajayaraman2606@gmail.com>
2021-03-03 00:22:31 +0530

    trademark changes

--- src/pages/Project IP1/projectIp.js
+++ src/pages/Project IP1/projectIp.js
@@ -1,156 +1,212 @@
-import react from 'react';
+import react, { useState } from 'react';
 import './projectIp.scss';
 import Grid from '@material-ui/core/Grid';
-import Tabs from '../../component/Tabs/tabs';
 import Labelbox from "../../helpers/labelbox/labelbox";
 import { TableContainer } from '@material-ui/core';
 import CustomButton from '../../component/Butttons/button';
+import { useDispatch, connect } from "react-redux";
+import ValidationLibrary from "../../helpers/validationfunction";
+import { InesertResume } from "../../actions/ResumeAction"
+import { Tabs } from 'antd';
+
+//import tab content:
+
+import TradeMark from './TradeMark/tradeMark';
+
+const { TabPane } = Tabs;
 
 
 
 function ProjectIp() {
+    const dispatch = useDispatch()
 
-    const TableContainer = () => {
-        return (
-            <div className="tradeMarkContainer">
-               
-                    <Grid item xs={12} container direction="row" spacing={1} >
-                        <Grid item xs={4} container direction="column" spacing={2} className="projectIp" >
-                            <Grid item xs={12} container direction="row" spacing={1}>
-                                <Grid item xs={4} >
-                                    <Labelbox type="select"
-                                        placeholder={" Status"} />
-                                </Grid>
-                                <Grid item xs={4} >
-                                    <Labelbox type="text"
-                                        placeholder={" Mark"} />
-                                </Grid>
-                                <Grid item xs={4} >
-                                    <Labelbox type="text"
-                                        placeholder={" Project Type"} />
-                                </Grid>
-                            </Grid>
-                            <Grid item xs={12} >
-                                <Labelbox type="text"
-                                    placeholder={" Goods and Description"} />
-                            </Grid>
-                            <Grid item xs={12} >
-                                <Labelbox type="text"
-                                    placeholder={" Internal Status"} />
-                            </Grid>
-                            <Grid item xs={12} >
-                                <Labelbox type="text"
-                                    placeholder={" Amendment"} />
-                            </Grid>
-                            <Grid item xs={12} >
-                                <Labelbox type="text"
-                                    placeholder={" Priority Details"} />
-                            </Grid>
-                            <Grid item xs={12} >
-                                <Labelbox type="select"
-                                    placeholder={" POA"} />
-                            </Grid>
-                            
-                        </Grid>
-                        <Grid item xs={4} container direction="column" spacing={2}>
-                            <Grid item xs={12} container direction="row" spacing={1}>
-                                <Grid item xs={6} >
-                                    <Labelbox type="text"
-                                        placeholder={" Application Number "} />
-                                </Grid>
-                                <Grid item xs={6} >
-                                    <Labelbox type="datepicker"
-                                        placeholder={" Application Date "} />
-                                </Grid>
-
-                            </Grid>
-                            <Grid item xs={12} container direction="row" spacing={1}>
-                                <Grid item xs={6} >
-                                    <Labelbox type="select"
-                                        placeholder={" Usage Details "} />
-                                </Grid>
-                                <Grid item xs={6} >
-                                    <Labelbox type="datepicker"
-                                        placeholder={" Used From Date "} />
-                                </Grid>
-
-                            </Grid>
-
-                            <Grid item xs={12} >
-                                <Labelbox type="text"
-                                    placeholder={" Allotment"} />
-                            </Grid>
-                            <Grid item xs={12} >
-                                <Labelbox type="text"
-                                    placeholder={" Order"} />
-                            </Grid>
-                            <Grid item xs={12} container direction="row" spacing={1}>
-                                <Grid item xs={6} >
-                                    <Labelbox type="text"
-                                        placeholder={" Usage Details "} />
-                                </Grid>
-                                <Grid item xs={6} >
-                                    <Labelbox type="datepicker"
-                                        placeholder={" Used From Date "} />
-                                </Grid>
-
-                            </Grid>
-                            <Grid item xs={12} >
-                                <Labelbox type="datepicker"
-                                    placeholder={" Certificate Date"} />
-                            </Grid>
-                            <Grid item xs={12} container justify="center" >
-                                <CustomButton btnName={"save"} btnCustomColor="customPrimary" />
-                                <CustomButton btnName={"cancel"} />
-
-                            </Grid>
-
-                        </Grid>
-
-                        <Grid item xs={4} container direction="column" spacing={2}>
-
-                            <Grid item xs={12} >
-                                <Labelbox type="select"
-                                    placeholder={" Class"} />
-                            </Grid>
-                            <Grid item xs={12} >
-                                <Labelbox type="text"
-                                    placeholder={" coments"} />
-                            </Grid>
-                            <Grid item xs={12} >
-                                <Labelbox type="text"
-                                    placeholder={" IP India Status"} />
-                            </Grid>
-                            <Grid item xs={12} >
-                                <Labelbox type="text"
-                                    placeholder={" Restrictions"} />
-                            </Grid>
-                            <Grid item xs={12} >
-                                <Labelbox type="text"
-                                    placeholder={" Restrictions"} />
-                            </Grid>
-                            <Grid item xs={12} >
-                                <Labelbox type="text"
-                                    placeholder={" Restrictions"} />
-                            </Grid>
-                            <Grid item xs={12} >
-                            <Labelbox type="text"
-                                placeholder={" Next Renewal Date"} />
-                        </Grid>
-
-                        </Grid>
-                      
-
-
-                    </Grid>
-
-
-
-                
-               
-            </div>
-        )
+    function callback(key) {
+        console.log(key);
     }
+
+    function callbackinside(key) {
+        console.log(key);
+    }
+
+
+    const [Resume_Form, setResumeFrom] = useState({
+
+        mark: {
+            value: "",
+            validation: [{ "name": "required" },],
+            error: null,
+            errmsg: null,
+        },
+        projecttype: {
+            value: "",
+            validation: [{ "name": "required" },],
+            error: null,
+            errmsg: null,
+        },
+        goodsdescription: {
+            value: "",
+            validation: [{ "name": "required" },],
+            error: null,
+            errmsg: null,
+        },
+        internalstutus: {
+            value: "",
+            validation: [{ "name": "required" },],
+            error: null,
+            errmsg: null,
+        },
+        amendment: {
+            value: "",
+            validation: [{ "name": "required" },],
+            error: null,
+            errmsg: null,
+        },
+        prioritydetails: {
+            value: "",
+            validation: [{ "name": "required" },],
+            error: null,
+            errmsg: null,
+        },
+        applicationNumber: {
+            value: "",
+            validation: [{ "name": "required" },],
+            error: null,
+            errmsg: null,
+        },
+        internalstutus: {
+            value: "",
+            validation: [{ "name": "required" },],
+            error: null,
+            errmsg: null,
+        },
+        allotment: {
+            value: "",
+            validation: [{ "name": "required" },],
+            error: null,
+            errmsg: null,
+        },
+        order: {
+            value: "",
+            validation: [{ "name": "required" },],
+            error: null,
+            errmsg: null,
+        },
+        usagedetails: {
+            value: "",
+            validation: [{ "name": "required" },],
+            error: null,
+            errmsg: null,
+        },
+        coments: {
+            value: "",
+            validation: [{ "name": "required" },],
+            error: null,
+            errmsg: null,
+        },
+        indiaStatus: {
+            value: "",
+            validation: [{ "name": "required" },],
+            error: null,
+            errmsg: null,
+        },
+        restrictions: {
+            value: "",
+            validation: [{ "name": "required" },],
+            error: null,
+            errmsg: null,
+        },
+        clientname: {
+            value: "",
+            validation: [{ "name": "required" },],
+            error: null,
+            errmsg: null,
+        },
+
+    })
+
+    function onSubmit() {
+        var mainvalue = {};
+        var targetkeys = Object.keys(Resume_Form);
+        for (var i in targetkeys) {
+            var errorcheck = ValidationLibrary.checkValidation(
+                Resume_Form[targetkeys[i]].value,
+                Resume_Form[targetkeys[i]].validation
+            );
+            Resume_Form[targetkeys[i]].error = !errorcheck.state;
+            Resume_Form[targetkeys[i]].errmsg = errorcheck.msg;
+            mainvalue[targetkeys[i]] = Resume_Form[targetkeys[i]].value;
+        }
+        var filtererr = targetkeys.filter(
+            (obj) => Resume_Form[obj].error == true
+        );
+        console.log(filtererr.length);
+        if (filtererr.length > 0) {
+            // setResumeFrom({ error: true });
+        } else {
+            // setResumeFrom({ error: false });
+
+            dispatch(InesertResume(Resume_Form)).then(() => {
+                handleCancel()
+            })
+        }
+
+        setResumeFrom(prevState => ({
+            ...prevState
+        }));
+    };
+
+    const handleCancel = () => {
+        let ResumeFrom_key = [
+            "mark", "projecttype", "goodsdescription", "internalstutus", "basicQualification", "additionalQualification1", "additionalQualification2", "institution", "lastEmployer", "startDate", "endDate", "email1", "email2", "phone1", "phone2", "skills", "Traits", "certifications", "specializations", "talents", "intrests", "contactPhone", "emailId", "mailAddress", "state", "city", "language", "industry"
+        ]
+
+        ResumeFrom_key.map((data) => {
+            Resume_Form[data].value = ""
+        })
+        setResumeFrom(prevState => ({
+            ...prevState,
+        }));
+    }
+
+
+
+    function checkValidation(data, key, multipleId) {
+
+        var errorcheck = ValidationLibrary.checkValidation(
+            data,
+            Resume_Form[key].validation
+        );
+        let dynObj = {
+            value: data,
+            error: !errorcheck.state,
+            errmsg: errorcheck.msg,
+            validation: Resume_Form[key].validation
+        }
+
+        // only for multi select (start)
+
+        let multipleIdList = []
+
+        if (multipleId) {
+            multipleId.map((item) => {
+                for (let i = 0; i < data.length; i++) {
+                    if (data[i] === item.value) {
+                        multipleIdList.push(item.id)
+                    }
+                }
+            })
+            dynObj.valueById = multipleIdList.toString()
+        }
+        // (end)
+
+        setResumeFrom(prevState => ({
+            ...prevState,
+            [key]: dynObj,
+        }));
+
+    };
+
+
 
     return (
         <div>
@@ -159,12 +215,20 @@ function ProjectIp() {
                     <Grid item xs={6}>
                         <Labelbox type="text"
                             placeholder={"Project Name *"}
+                        // changeData={(data) => checkValidation(data, "projectname")}
+                        // value={Resume_Form.projectname.value}
+                        // error={Resume_Form.projectname.error}
+                        // errmsg={Resume_Form.projectname.errmsg}
 
                         />
                     </Grid>
                     <Grid item xs={6}>
                         <Labelbox type="text"
                             placeholder={"client Name *"}
+                            changeData={(data) => checkValidation(data, "clientname")}
+                            value={Resume_Form.clientname.value}
+                            error={Resume_Form.clientname.error}
+                            errmsg={Resume_Form.clientname.errmsg}
 
                         />
                     </Grid>
@@ -224,11 +288,28 @@ function ProjectIp() {
 
 
             </Grid>
-            <Tabs tabHeading="Intellectual Property" tabContent={<Tabs className="tabs" tabHeading="Trade Mark" tabContent={TableContainer()} keys={1} />} >
+            <Tabs onChange={callback} type="card">
+                <TabPane tab="Tab 1" key="1">
+                    <Tabs onChange={callbackinside} type="card" className="tradeMarkTab">
+                        <TabPane tab="Trade Mark" key="1">
+                            <TradeMark />
+                        </TabPane>
+                        <TabPane tab="Patent" key="2">
+                            Content of Tab Pane 2
+                </TabPane>
+                        <TabPane tab="Design" key="3">
+                            Content of Tab Pane 3
+                </TabPane>
+                        <TabPane tab="CopyRight" key="3">
+                            Content of Tab Pane 3
+                </TabPane>
 
 
+                    </Tabs>
+                </TabPane>
 
             </Tabs>
+
 
         </div>
     )
