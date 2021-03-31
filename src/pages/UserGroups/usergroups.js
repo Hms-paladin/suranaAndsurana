dc0f1e7bbc0b2203da8fb8fd2bb5339607713e90
Jaison Lal <jaison@paladinsoftwares.com>
2021-03-31 16:02:56 +0530

    usergroup ui design

--- src/pages/UserGroups/usergroups.js
+++ src/pages/UserGroups/usergroups.js
@@ -1,53 +1,141 @@
-import React from 'react'
-import Grid from "@material-ui/core/Grid";
+import React, { useEffect, useState } from 'react'
+import Grid from '@material-ui/core/Grid';
 import Labelbox from "../../helpers/labelbox/labelbox";
-import PlusIcon from "../../images/plusIcon.svg";
+import CustomButton from '../../component/Butttons/button';
 
-const UserGroups = () => {
-  
+import EnhancedTable from '../../component/DynTable/table';
+import PlusIcon from "../../images/plusIcon.svg";
+import DynModel from "../../component/Model/model";
+import './usergroups.scss'
+import { Checkbox } from 'antd';
+import Edit from "../../images/pencil.svg";
+
+const UserGroups = (props) => {
+  const header = [
+    // { id: 'table_name', label: 'Table Name' },
+    { id: 'employee', label: 'Employee' },
+    { id: 'group', label: 'Group' },
+    { id: 'edit', label: 'Edit' },
+  ];
+
+  const [UserGroupsList, setUserGroupsList] = useState([])
+  const [usergroupmodel, setUsergroupmodel] = useState(false);
+
+  const [isLoaded, setIsLoaded] = useState(true);
+  useEffect(() => {
+
+    if (isLoaded) {
+
+        var groupList = [];
+
+        var listarray = {
+          "project_type": "Kaveri",
+          "sub_project_type":"Interviewer,Employee",
+          "process_type": <img src={Edit} style={{cursor: 'pointer',width:19}} onClick={()=>setUsergroupmodel(true)} />,
+        }
+        groupList.push(listarray);
+        listarray = {
+          "project_type": "Pradish",
+          "sub_project_type": "Interviewer,Employee",
+          "process_type": <img src={Edit} style={{cursor: 'pointer',width:19}} onClick={()=>setUsergroupmodel(true)} />,
+        }
+        groupList.push(listarray);
+        
+        setUserGroupsList({ groupList })
+
+      setIsLoaded(false);
+    }
+
+  })
+
+
+  function checkValidation(data, key, multipleId) {
+
+    if (data && key  == "group") {
+
+     console.log("group")
+    }
+    if (data && key  == "employee") {
+
+      console.log("employee")
+     }
+
+
+  }
+
+ 
   return (
     <div>
-      <Grid item xs={6} className="ContentTitle">
-        User Groups
-      </Grid>
-      <div className="Container">
-        {/* <div className="leftContainer"> */}
-          <Grid container 
-           direction="row"
-           className="spaceBtGrid"
-           alignItems="center"
-           spacing={3}>
-            <Grid item xs={3}>
-              <Labelbox
-                type="select"
-                placeholder={"Employee"}
-              // dropdown={resumeGetList.candidateList}
-              // changeData={(data) => checkValidation(data, "candidate")}
-              // value={Resume_Form.candidate.value}
-              // error={Resume_Form.candidate.error}
-              // errmsg={Resume_Form.candidate.errmsg}
-              />
-            </Grid>
-
-            <Grid item xs={3}>
-              <Labelbox
-                type="select"
-                placeholder={"Employee"}
-              // dropdown={resumeGetList.candidateList}
-              // changeData={(data) => checkValidation(data, "candidate")}
-              // value={Resume_Form.candidate.value}
-              // error={Resume_Form.candidate.error}
-              // errmsg={Resume_Form.candidate.errmsg}
-              />
-            </Grid>
-            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-              <img src={PlusIcon} style={{ cursor: 'pointer', width: 19 }} />
-            </div>
+    <Grid container spacing={2} className="ratemaster_firstgrid">
+        <Grid
+          item
+          xs={7}
+          container
+          direction="row"
+          className="spaceBtGrid"
+          alignItems="center"
+        >
+          <Grid item xs={6}>
+          <Labelbox type="select" placeholder={"Employee"}
+           
+            dropdown=""
+            changeData={(data) => checkValidation(data, "employee")}
+            
+          />
+           </Grid>
+          <Grid item xs={6}>
+          <Labelbox type="select" placeholder={"Group"}
+            
+            changeData={(data) => checkValidation(data, "group")}
+            dropdown=""
+            
+          />
           </Grid>
-        {/* </div> */}
+        </Grid>
+        <div style={{display: 'flex',justifyContent: 'flex-end'}}>
+          <img src={PlusIcon} style={{cursor: 'pointer',width:19,marginTop: -23}}  />
+          </div>
+       
+      </Grid>
+
+      <div className="rate_enhanced_table">
+        <EnhancedTable headCells={header}
+          rows={UserGroupsList.length == 0 ? UserGroupsList : UserGroupsList.groupList} />
       </div>
+      <DynModel
+          modelTitle={"Edit Group Membership"}
+          handleChangeModel={usergroupmodel}
+          handleChangeCloseModel={(bln) => setUsergroupmodel(bln)}
+          content={
+            <div className="successModel">
+
+              <div> <label className="usergroup_label">Employee :&nbsp;Kaveri</label></div>
+              <div className="usergroupmodelDiv">
+                <div className="usergroupcheckboxDiv"><Checkbox  /> &nbsp;&nbsp;<label style={{color:'black'}}>Interview Approval</label> </div>
+                <div  className="usergroupcheckboxDiv"> <Checkbox  />&nbsp;&nbsp;<label style={{color:'black'}}>Interviewer</label> </div>
+                <div  className="usergroupcheckboxDiv"> <Checkbox  />&nbsp;&nbsp;<label style={{color:'black'}}>HR Assistant</label> </div>
+              </div>
+              <div className="customUsergroupbtn">
+                <CustomButton
+                  btnName={"Save"}
+                  btnCustomColor="customPrimary"
+                  custombtnCSS={"btnUsergroup"}
+                  onBtnClick={()=>setUsergroupmodel(false)}
+                />
+                <CustomButton
+                 btnName={"Cancel "} 
+                 custombtnCSS={"btnUsergroup"}
+                 onBtnClick={()=>setUsergroupmodel(false)}
+                  />
+              </div>
+            </div>
+
+          }
+          width={400}
+        />
     </div>
   )
 }
 
-export default UserGroups
+
+export default (UserGroups);
