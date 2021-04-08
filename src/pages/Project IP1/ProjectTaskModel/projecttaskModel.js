783dceea7869fdde7ac43001a415438fe404bfda
Ajith Kumar K <ajithkumar@itechservicesplc.com>
2021-04-08 10:51:08 +0530

    Adhoc task and task api integration

--- src/pages/Project IP1/ProjectTaskModel/projecttaskModel.js
+++ src/pages/Project IP1/ProjectTaskModel/projecttaskModel.js
@@ -1,18 +1,94 @@
-import react, { useState } from 'react';
+import react, { useState, useEffect } from 'react';
 import './projectModel.scss';
 import Labelbox from "../../../helpers/labelbox/labelbox";
 import Grid from '@material-ui/core/Grid';
 import CustomButton from '../../../component/Butttons/button';
-import { useDispatch, connect } from "react-redux";
+import { apiurl } from "../../../utils/baseUrl";
+import { connect, useDispatch } from "react-redux";
+import { getActivity,getPriorityList,getTagList,inserTask,getAssignedTo,getLocation } from "../../../actions/projectTaskAction";
+import Axios from "axios";
 import ValidationLibrary from "../../../helpers/validationfunction";
 import { InesertResume } from "../../../actions/ResumeAction";
+function ProjectTaskModel(props) {
+    
+    const dispatch = useDispatch();
+    const [projectSubActivity, setprojectSubActivity] = useState({});
+    const [activityList, setactivityList] = useState({})
+    const [priorityList, setpriorityList] = useState({})
+    const [taggList, settaggList] = useState({})
+    const [assignedToLists, setassignedToLists] = useState({}) 
+    const [locationslList, setlocationslList] = useState({})
+
+    const [InsertTaskForm, setInsertTaskForm] = useState({
+        activity: {
+          value: "",
+          //validation: [{ name: "required" }],
+          error: null,
+          errmsg: null,
+        }, 
+        subActivity: {
+          value: "",
+          validation: [{ name: "required" }],
+          error: null,
+          errmsg: null,
+        },
+        location: {
+          value: "",
+          validation: [{ name: "required" }],
+          error: null,
+          errmsg: null,
+        },
+        fromDate: {
+          value: "",
+          validation: [{ name: "required" }],
+          error: null,
+          errmsg: null,
+        },
+        toDate: {
+          value: "",
+          validation: [{ name: "required" }],
+          error: null,
+          errmsg: null,
+        },
+        assignTo: {
+          value: "",
+          validation: [{ name: "required" }],
+          error: null,
+          errmsg: null,
+        },
+        tag: {
+          value: "",
+          valueById: "",
+          validation: [{ name: "required" }],
+          error: null,
+          errmsg: null,
+        },
+        priority: {
+          value: "",
+          valueById: "",
+          validation: [{ name: "required" }],
+          error: null,
+          errmsg: null,
+        },
+        description: {
+          value: "",
+          valueById: "",
+          validation: [{ name: "required" }],
+          error: null,
+          errmsg: null,
+        }
+    });
+    
+      useEffect(() => {
+        dispatch(getActivity());
+        dispatch(getTagList());
+        dispatch(getPriorityList());
+        dispatch(getAssignedTo());
+        dispatch(getLocation());
+        
+      }, []);
 
 
-
-
-function ProjectTaskModel() {
-
-    const dispatch = useDispatch()
     const [ProjectTask_Model, setResumeFrom] = useState({
 
         startdate: {
@@ -118,6 +194,144 @@ function ProjectTaskModel() {
         }));
 
     };
+      useEffect(() => {
+        let activityTypeData = []
+    props.activitysList.map((data) =>
+    activityTypeData.push({ value: data.activity,
+        id: data.activity_id })
+    )
+    setactivityList({ activityTypeData })
+
+
+    let priorityTypeData = []
+    props.prioritysList.map((data) =>
+    priorityTypeData.push({ value: data.status,
+        id: data.status_id })
+    )
+    setpriorityList({ priorityTypeData })
+
+    let tagTypeData = []
+    props.tagsList.map((data) =>
+    tagTypeData.push({ value: data.status,
+        id: data.status_id })
+    )
+    settaggList({ tagTypeData })
+
+    let locationData = []
+    props.locationList.map((data) =>
+    locationData.push({ value: data.location,
+        id: data.location_id })
+    )
+    setlocationslList({ locationData })
+
+    let assignedToData = []
+    props.assignToList.map((data) =>
+    assignedToData.push({ value: data.name,
+        id: data.emp_id })
+    )
+    setassignedToLists({ assignedToData })
+
+   
+     
+
+      }, [
+        props.activitysList,props.prioritysList,props.tagsList,props.locationList,props.assignToList
+      ]);
+
+      function fnLoadSubActivity(data, key) {
+        if (key == "activity") {
+            // Sub Activity
+            Axios({
+              method: "POST",
+              url: apiurl + "get_sub_activity",
+              data: {
+                activity_id: data,
+              },
+            }).then((response) => {
+              let projectSubActivitydata = [];
+              response.data.data.map((data) =>
+                projectSubActivitydata.push({
+                  value: data.sub_activity,
+                  id: data.sub_activity_id,
+                })
+              );
+              setprojectSubActivity({ projectSubActivitydata });
+            });
+          }
+      }
+
+      function onSubmit(){
+          var data ={
+            "project_id":"71",
+            "activiity_id":InsertTaskForm.activity.value,
+            "sub_activity_id":InsertTaskForm.subActivity.value,
+            "assignee_id":InsertTaskForm.assignTo.value,
+            "start_date":InsertTaskForm.fromDate.value,
+            "end_date":InsertTaskForm.toDate.value,
+            "assigned_by":localStorage.getItem("empId"),
+            "priority":InsertTaskForm.priority.value,
+            "description":InsertTaskForm.description.value,
+            "tag":InsertTaskForm.tag.value
+        }
+
+        dispatch(inserTask(data)).then((response) => {
+            console.log("Insert");
+            //onStateClear()
+        })
+
+      }
+
+      function checkValidation(data, key, multipleId) {
+        var errorcheck = ValidationLibrary.checkValidation(
+          data,
+          InsertTaskForm[key].validation
+        );
+        let dynObj = {
+          value: data,
+          error: !errorcheck.state,
+          errmsg: errorcheck.msg,
+          validation: InsertTaskForm[key].validation,
+        };
+    
+        // only for multi select (start)
+    
+        let multipleIdList = [];
+    
+        if (multipleId) {
+          multipleId.map((item) => {
+            for (let i = 0; i < data.length; i++) {
+              if (data[i] === item.value) {
+                multipleIdList.push(item.id);
+              }
+            }
+          });
+          dynObj.valueById = multipleIdList.toString();
+        }
+        // (end)
+        if (key == "activity") {
+            // Sub Activity
+            Axios({
+              method: "POST",
+              url: apiurl + "get_sub_activity",
+              data: {
+                activity_id: data,
+              },
+            }).then((response) => {
+              let projectSubActivitydata = [];
+              response.data.data.map((data) =>
+                projectSubActivitydata.push({
+                  value: data.sub_activity,
+                  id: data.sub_activity_id,
+                })
+              );
+              setprojectSubActivity({ projectSubActivitydata });
+            });
+          }
+        setInsertTaskForm((prevState) => ({
+          ...prevState,
+          [key]: dynObj,
+        }));
+      }
     return (
         <div className="projectTaskModel">
 
@@ -131,25 +345,38 @@ function ProjectTaskModel() {
             <div className="activityTask">
                 <Grid item xs={7} >
                     <Labelbox type="select"
-                        placeholder={"Activity"} />
+                    dropdown={activityList.activityTypeData}
+                    changeData={(data) => checkValidation(data, "activity")}
+                    //changeData={(data) => fnLoadSubActivity(data, "activity")}
+                        placeholder={"Activity"} 
+                        value={InsertTaskForm.activity.value}
+                        error={InsertTaskForm.activity.error}
+                        errmsg={InsertTaskForm.activity.errmsg}
+                        />
                 </Grid>
             </div>
             <div className="activityTask">
                 <Grid item xs={7} >
                     <Labelbox type="select"
-                        placeholder={"Sub Activity"} />
+            dropdown={projectSubActivity.projectSubActivitydata}
+            changeData={(data) => checkValidation(data, "subActivity")}
+                        placeholder={"Sub Activity"} 
+                        value={InsertTaskForm.subActivity.value} />
                 </Grid>
             </div>
             <div className="activityTask">
                 <Grid item xs={7} >
-                    <Labelbox type="select"
+                    <Labelbox type="select" value={InsertTaskForm.location.value}
+                     dropdown={locationslList.locationData}
+                    changeData={(data) => checkValidation(data, "location")}
                         placeholder={"Location"} />
                 </Grid>
             </div>
             <div className="projectTaskDatealign">
                 <Grid container spacing={3}>
                     <Grid item xs={4} >
-                        <Labelbox type="datepicker"
+                        <Labelbox type="datepicker" value={InsertTaskForm.fromDate.value}
+                        changeData={(data) => checkValidation(data, "fromDate")}
                             placeholder={"Start Date"}
                             changeData={(data) => checkValidation(data, "startdate")}
                             value={ProjectTask_Model.startdate.value}
@@ -159,7 +386,8 @@ function ProjectTaskModel() {
                         />
                     </Grid>
                     <Grid item xs={4} >
-                        <Labelbox type="datepicker"
+                        <Labelbox type="datepicker" value={InsertTaskForm.toDate.value}
+                        changeData={(data) => checkValidation(data, "toDate")}
                             placeholder={" End Date"}
                             changeData={(data) => checkValidation(data, "enddate")}
                             value={ProjectTask_Model.enddate.value}
@@ -169,7 +397,10 @@ function ProjectTaskModel() {
                         />
                     </Grid>
                     <Grid item xs={4} >
-                        <Labelbox type="select"
+                        <Labelbox type="select" value={InsertTaskForm.assignTo.value}
+
+                     dropdown={assignedToLists.assignedToData}
+                        changeData={(data) => checkValidation(data, "assignTo")}
                             placeholder={"Assign To"} />
                     </Grid>
 
@@ -180,23 +411,28 @@ function ProjectTaskModel() {
                     <Grid item xs={7}>
                         <div className="projectTaskCmd">
                             <Labelbox type="textarea"
-                                placeholder={"Description"}
+                                placeholder={"Description"} 
                                 changeData={(data) => checkValidation(data, "description")}
-                                value={ProjectTask_Model.description.value}
-                                error={ProjectTask_Model.description.error}
-                                errmsg={ProjectTask_Model.description.errmsg} />
+                                value={InsertTaskForm.description.value}
+                                />
                         </div>
                     </Grid>
 
                     <Grid item xs={5} >
                         <Grid item xs={12} >
-                            <Labelbox type="select"
+                            <Labelbox type="select" value={InsertTaskForm.tag.value}
+changeData={(data) => checkValidation(data, "tag")}
+    dropdown={taggList.tagTypeData}
                                 placeholder={"Tag"} />
                         </Grid>
 
                         <Grid item xs={12} >
-                            <Labelbox type="select"
-                                placeholder={"Priority"} />
+                            <Labelbox type="select" 
+    dropdown={priorityList.priorityTypeData}
+    changeData={(data) => checkValidation(data, "priority")}
+                                placeholder={"Priority"}  
+                        value={InsertTaskForm.priority.value}
+                                />
                         </Grid>
 
                     </Grid>
@@ -204,7 +440,7 @@ function ProjectTaskModel() {
             </div>
             <div className="projectTaskModelButtons">
                 <CustomButton btnName={"CANCEL"} custombtnCSS={"projectTaskGo"} />
-                <CustomButton btnName={"SAVE"} btnCustomColor="customPrimary" custombtnCSS={"projectTaskGo"} onBtnClick={onSubmit} />
+                <CustomButton btnName={"SAVE"} btnCustomColor="customPrimary" onBtnClick={onSubmit} custombtnCSS={"projectTaskGo"} />
 
             </div>
 
@@ -214,4 +450,15 @@ function ProjectTaskModel() {
     )
 }
 
-export default ProjectTaskModel;
+const mapStateToProps = (state) =>
+// console.log(state.getOptions.getProcessType, "getProcessType")
+({
+    
+    activitysList: state.projectTasksReducer.getActivityList || [],
+    prioritysList: state.projectTasksReducer.prioritysList || [],
+    tagsList: state.projectTasksReducer.tagsList || [],
+    assignToList: state.projectTasksReducer.assignToLists || [],
+    locationList: state.projectTasksReducer.locationLists || [],
+});
+
+export default connect(mapStateToProps)(ProjectTaskModel);
---
+++ src/reducers/projectTasksReducer.js
@@ -0,0 +1,28 @@
+import { GET_ACTIVITY, GET_PRIORITY,GET_TAG,INSERT_TASK,INSERT_ADHOC_TASK,GET_ASSIGN_TO,GET_LOCATION}  from '../utils/Constants.js'
+
+const intialState = {
+    getActivityList: [], prioritysList:[],tagsList:[],insertTask :[], insertAdhocTask :[] ,  assignToLists :[],locationLists :[]
+}
+
+export default function (state = intialState, action) {
+    const { type, payload } = action;
+    switch (type) {
+        case GET_ACTIVITY:
+            return  { ...state, getActivityList: payload } 
+            case GET_TAG:
+            return  { ...state, tagsList: payload } 
+            case GET_PRIORITY:
+            return  { ...state, prioritysList: payload }
+            case INSERT_TASK:
+                return  { ...state, insertTask: payload }
+            case INSERT_ADHOC_TASK:
+            return  { ...state, insertAdhocTask: payload }
+            case GET_ASSIGN_TO:
+            return  { ...state, assignToLists: payload }
+            case GET_LOCATION:
+            return  { ...state, locationLists: payload }
+        default:
+            return state;
+    }
+
+}
