aa48f4fe9052eeec90e14f7228d85e79ff34f939
Asraf <mohammedasraf11z@gmail.com>
2021-04-17 12:06:32 +0530

    loginApiResponsechanges

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
   const [authTokens, setAuthTokens] = useState(existingTokens);
 
   const setTokens = (data) => {
-    localStorage.setItem("token", JSON.stringify(data.data[0][0]));
-    localStorage.setItem("empId", JSON.stringify(data.data[0][0].emp_id));
-    localStorage.setItem("user_id", JSON.stringify(data.data[0][0].user_id));
-    localStorage.setItem("user_name", JSON.stringify(data.data[0][0].user_name));
+    localStorage.setItem("token", JSON.stringify(data.data[0]));
+    localStorage.setItem("empId", JSON.stringify(data.data[0].emp_id));
+    localStorage.setItem("user_id", JSON.stringify(data.data[0].user_id));
+    localStorage.setItem("user_name", JSON.stringify(data.data[0].user_name));
     setAuthTokens(data);
   }
 
