5767e54782f888947be1ae6b07c5364df3c45643
unknown <birundhajayaraman2606@gmail.com>
2021-04-23 19:11:25 +0530

    working on user master api

--- src/actions/UserGroupAction.js
+++ src/actions/UserGroupAction.js
@@ -1,5 +1,5 @@
 
-import { INSERT_USERGROUP ,GET_GROUPNAME} from "../utils/Constants";
+import { INSERT_USERGROUP, GET_GROUPNAME, UPDATE_GROUP_NAME, DELETE_GROUPNAME } from "../utils/Constants";
 import { apiurl } from "../utils/baseUrl.js";
 import axios from "axios";
 import { notification } from 'antd'
@@ -33,7 +33,7 @@ export const InsertUsergroup = (UserGroup, groupName) => async dispatch => {
             data: {
 
                 "group_name": UserGroup.groupname.value || 0,
-                "created_by": "1",
+                "created_by": localStorage.getItem("empId"),
                 "group_id": "0"
             },
         }).then((response) => {
@@ -41,6 +41,40 @@ export const InsertUsergroup = (UserGroup, groupName) => async dispatch => {
                 dispatch({ type: INSERT_USERGROUP, payload: true })
                 notification.success({
                     message: " inserted Successfully",
+                });
+                dispatch(getGroupName())
+                return Promise.resolve();
+            }
+        });
+
+    } catch (err) {
+
+    }
+}
+
+export const updateGroupName = (UserGroup) => async dispatch => {
+    try {
+        axios({
+            method: 'PUT',
+            url: apiurl + 'updateGroupMaster',
+            data: {
+
+                "group_name": UserGroup.groupname.value || 0,
+                "created_by": localStorage.getItem("empId"),
+                "group_id": UserGroup.groupid.value || 0,
+            },
+        }).then((response) => {
+            if (response.data.status === 1) {
+                notification.success({
+                    message: "updated sucessfully",
+                });
+                dispatch({ type: UPDATE_GROUP_NAME, payload: response.data.status })
+                // dispatch(getLeaveBalance(params,employee_code))
+                dispatch(getGroupName())
+                return Promise.resolve();
+            } else {
+                notification.success({
+                    message: response.data.data,
                 });
                 return Promise.resolve();
             }
@@ -50,4 +84,36 @@ export const InsertUsergroup = (UserGroup, groupName) => async dispatch => {
 
     }
 }
+
+export const deleteGroupName = (deleteID) => async dispatch => {
+    console.log(deleteID,"deleteID")
+    try {
+        axios({
+            method: 'DELETE',
+            url: apiurl + 'deleteGroupMaster',
+            data: {
+                "group_id": deleteID || 0,
+            },
+        }).then((response) => {
+            if (response.data.status === 1) {
+                notification.success({
+                    message: "Deleted sucessfully",
+                });
+                dispatch({ type: DELETE_GROUPNAME, payload: response.data.status })
+                // dispatch(getLeaveBalance(params,employee_code))
+                dispatch(getGroupName())
+                return Promise.resolve();
+            } else {
+                notification.success({
+                    message: response.data.data,
+                });
+                return Promise.resolve();
+            }
+        });
+
+    } catch (err) {
+
+    }
+}
+
 
