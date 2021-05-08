ae27871fed94221c53532b201b4a382785d33713
admin <admin@admins-MacBook-Air.local>
2021-05-08 10:43:40 +0530

    group control

--- src/reducers/UserGroupReducer.js
+++ src/reducers/UserGroupReducer.js
@@ -1,5 +1,6 @@
 
-import { INSERT_USERGROUP, GET_GROUPNAME, UPDATE_GROUP_NAME, DELETE_GROUPNAME } from '../utils/Constants.js';
+import { INSERT_USERGROUP, GET_GROUPNAME, UPDATE_GROUP_NAME, DELETE_GROUPNAME, GET_GROUP_LIST,GET_EMP_LIST,GET_GROUP_CONTROL_LIST,
+    GET_EMP_GROUP_LIST } from '../utils/Constants.js';
 // const initalState = {
 //     getProject_type:[],
 
@@ -10,7 +11,8 @@ const intialState = {
     InsertUsergroup: [],
     getGroupName: [],
     updateGroupName: [],
-    deleteGroupMaster: []
+    deleteGroupMaster: [],
+    groupLists :[], employeeLists :[], employeeGroupDetLists :[] , getGroupControlLists :[]
 
 }
 
@@ -27,6 +29,14 @@ export default function (state = [], action) {
             return { ...state, updateGroupName: payload }
         case DELETE_GROUPNAME:
             return { ...state, deleteGroupMaster: payload }
+            case GET_GROUP_LIST:
+                return { ...state, groupLists: payload }
+            case GET_EMP_LIST:
+                return { ...state, employeeLists: payload }
+                case GET_EMP_GROUP_LIST:
+                return { ...state, employeeGroupDetLists: payload }
+                case GET_GROUP_CONTROL_LIST:
+                return { ...state, getGroupControlLists: payload }
         default:
             return state;
     }
