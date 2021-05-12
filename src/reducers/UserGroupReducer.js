e77e7243d16ddce64ce2c7649ae7eb0adfaee89b
admin <admin@admins-MacBook-Air.local>
2021-05-12 19:35:50 +0530

    mgff

--- src/reducers/UserGroupReducer.js
+++ src/reducers/UserGroupReducer.js
@@ -1,6 +1,7 @@
 
-import { INSERT_USERGROUP, GET_GROUPNAME, UPDATE_GROUP_NAME, DELETE_GROUPNAME, GET_GROUP_LIST,GET_EMP_LIST,GET_GROUP_CONTROL_LIST,
-    GET_EMP_GROUP_LIST } from '../utils/Constants.js';
+import { INSERT_USERGROUP, GET_GROUPNAME, UPDATE_GROUP_NAME, DELETE_GROUPNAME,
+    GET_GROUP_EMP, GET_GROUP_LIST,GET_EMP_LIST,GET_GROUP_CONTROL_LIST,EDIT_GROUP_NAME,
+    GET_EMP_GROUP_LIST,EDIT_GROUP_CONTROL,GET_CONTROL_LIST,INSERT_GROUP_CONTROL } from '../utils/Constants.js';
 // const initalState = {
 //     getProject_type:[],
 
@@ -10,9 +11,10 @@ import { INSERT_USERGROUP, GET_GROUPNAME, UPDATE_GROUP_NAME, DELETE_GROUPNAME, G
 const intialState = {
     InsertUsergroup: [],
     getGroupName: [],
+    getControl:[],
     updateGroupName: [],
     deleteGroupMaster: [],
-    groupLists :[], employeeLists :[], employeeGroupDetLists :[] , getGroupControlLists :[]
+    groupLists :[], employeeLists :[], employeeGroupDetLists :[] , getGroupControlLists :[],getGroupsForEmp:[]
 
 }
 
@@ -25,6 +27,8 @@ export default function (state = [], action) {
             return { ...state, InsertUsergroup: payload }
         case GET_GROUPNAME:
             return { ...state, getGroupName: payload }
+        case GET_CONTROL_LIST:
+                return { ...state, getControl: payload }              
         case UPDATE_GROUP_NAME:
             return { ...state, updateGroupName: payload }
         case DELETE_GROUPNAME:
@@ -37,6 +41,14 @@ export default function (state = [], action) {
                 return { ...state, employeeGroupDetLists: payload }
                 case GET_GROUP_CONTROL_LIST:
                 return { ...state, getGroupControlLists: payload }
+                case GET_GROUP_EMP:
+                    return { ...state, getGroupsForEmp: payload }
+                case  EDIT_GROUP_NAME:
+                    return { ...state, editEmployeeGroup: payload }  
+                case  EDIT_GROUP_CONTROL:
+                        return { ...state, editGroupControl: payload }  
+                case INSERT_GROUP_CONTROL:
+                return { ...state, InsertGroupControlMaster: payload }  
         default:
             return state;
     }
