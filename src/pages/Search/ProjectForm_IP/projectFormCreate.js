74ed9c1c2e346b7c536ea35fa6fac52c0b3d5820
Ashwinkk797 <ashwinkk797@gmail.com>
2021-07-28 10:43:56 +0530

    DashboardFix and Check name

--- src/pages/Search/ProjectForm_IP/projectFormCreate.js
+++ src/pages/Search/ProjectForm_IP/projectFormCreate.js
@@ -77,6 +77,7 @@ function ProjectFormCreate(props) {
   const [proj_type_name, setProj_type_name] = useState();
   const [billable, setBillable] = useState();
   const [notfoundmodel, setNotfoundmodel] = useState(false);
+  const [projectExists,setProjectExists] = useState(1)
   const [projectform, setprojectform] = useState({
     client: {
       value: "",
@@ -237,8 +238,23 @@ function ProjectFormCreate(props) {
     // }
   };
 
+   async function checkProjectNameExists(projectName) {
+    //For client duplication validation 
+   await Axios({
+      method: "POST",
+      url: apiurl + "get_project_name_check",
+      data: {
+        "project_name": projectName,
+      }
+    }).then((response) => {
+      console.log(response.data)
+      setProjectExists(response.data.status)
+    });
+  }
 
-  function checkValidation(data, key, multipleId) {
+
+   function checkValidation(data, key, multipleId) {
+     checkProjectNameExists(projectform.projectname.value)
     var errorcheck = ValidationLibrary.checkValidation(
       data,
       projectform[key].validation
@@ -249,11 +265,38 @@ function ProjectFormCreate(props) {
       errmsg: errorcheck.msg,
       validation: projectform[key].validation,
     };
-
+ 
+    console.log(projectExists,"projectExists")  
+    
+    if(key==="projectname"){
+      if(projectExists===0){
+        dynObj = {
+          value: data,
+          error: !errorcheck.state,
+          errmsg: errorcheck.msg,
+          validation: [{ name: "required" },{name:"checkNameExists",params:0}  ],
+        };
+        setprojectform((prevState) => ({
+          ...prevState,
+          [key]: dynObj
+        }));
+     }else{
+      dynObj = {
+        value: data,
+        error: !errorcheck.state,
+        errmsg: errorcheck.msg,
+        validation: [{ name: "required" },{name:"checkNameExists",params:1}  ],
+      };
+      setprojectform((prevState) => ({
+        ...prevState,
+        [key]: dynObj
+      }));
+    }
+  }
+    console.log(dynObj,"dynObj")
     console.log(key, data, "key")
 
     if (data === 1 && key === "project_type") {
-
       let IP_project_key = [
         "client",
         "project_type",
@@ -267,7 +310,6 @@ function ProjectFormCreate(props) {
         "projectname",
         "process_type",
         "comments",
-
       ];
 
       IP_project_key.map((data) => {
@@ -328,10 +370,6 @@ function ProjectFormCreate(props) {
         }
       });
       hideValidation(["project_Subtype", "process_type"])
-
-
-
-
     }
 
     if (data && key === "project_Subtype") {
