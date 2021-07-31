4239ea655cb9a1fb2ab86b40731c4d4b4271a053
KaveriT-dotnet <kaveri2ganga@gmail.com>
2021-07-31 16:57:06 +0530

    changes

--- src/reducers/projectTasksReducer.js
+++ src/reducers/projectTasksReducer.js
@@ -1,25 +1,15 @@
 import {
     GET_ACTIVITY, GET_PRIORITY, GET_TAG, INSERT_TASK, INSERT_BACK_LOG, INSERT_ADHOC_TASK, GET_TIMESHEET_BY_TASK,
     GET_ASSIGN_TO, GET_LOCATION, INSERT_TIME_SHEET, GET_EXPENSE_TYPE,
-<<<<<<< HEAD
      GET_PAYMENT_MODE, GET_STAGESBY_PROJECT, GET_SUBSTAGES, GET_PROJECTSTAGES,GET_PROJECT_STAGES_LIST,GET_TASK_TIME_SHEET,
      GET_TASK_LIST,GET_HEARING_DETS,GET_ADJOURN_DET,INSERT_ADJOURN,INSERT_HEARING,GET_ADJOURN_TAKEN_BY
-=======
-    GET_PAYMENT_MODE, GET_STAGESBY_PROJECT, GET_SUBSTAGES, GET_PROJECTSTAGES, GET_PROJECT_STAGES_LIST, GET_TASK_TIME_SHEET,
-    GET_TASK_LIST, GET_HEARING_DETS, GET_ADJOURN_DET, INSERT_ADJOURN, INSERT_HEARING
->>>>>>> 7e357b3d25a7de2d769c2cfd80d5772b0cca75da
 } from '../utils/Constants.js'
 
 const intialState = {
     getProjectStageList: [], getActivityList: [], prioritysList: [], tagsList: [], insertTask: [], getTaskLists: [],
     insertAdhocTask: [], assignToLists: [], locationLists: [], stagesList: [], SubStagesList: [], getAllStage: [],
-<<<<<<< HEAD
     getTaskTimeSheet :[],getHearingDets :[],getAdjournDets :[],InsertHearingDets:[],InsertAdjournDets :[],getTaskTimeSheetbyTaskId:[],
     backLog:[],getAdjournTakenBy:[]
-=======
-    getTaskTimeSheet: [], getHearingDets: [], getAdjournDets: [], InsertHearingDets: [], InsertAdjournDets: [], getTaskTimeSheetbyTaskId: [],
-    backLog: []
->>>>>>> 7e357b3d25a7de2d769c2cfd80d5772b0cca75da
 }
 
 export default function (state = intialState, action) {
@@ -68,13 +58,9 @@ export default function (state = intialState, action) {
         case INSERT_HEARING:
             return { ...state, InsertAdjournDets: payload }
         case GET_TIMESHEET_BY_TASK:
-<<<<<<< HEAD
             return { ...state, getTaskTimeSheetbyTaskId: payload } 
         case GET_ADJOURN_TAKEN_BY:
             return { ...state, getAdjournTakenBy: payload }   
-=======
-            return { ...state, getTaskTimeSheetbyTaskId: payload }
->>>>>>> 7e357b3d25a7de2d769c2cfd80d5772b0cca75da
         default:
             return state;
     }
